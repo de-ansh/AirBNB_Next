efe880594c0491713964e266438f5109e4702e0b
Ashish <anshm9448@gmail.com>
2023-08-10 23:38:20 +0530

    Next Airbnb

---
+++ app/components/Container.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+
+interface ContainerProps{
+    children: React.ReactNode
+}
+
+const Container: React.FC<ContainerProps> = ({children}) => {
+    return ( 
+        <div className="max-w-{250px} mx-auto xl:px-20 sm: px-2 md:px-10 px-4 ">
+             {children}
+        </div>
+     );
+}
+ 
+export default Container;
