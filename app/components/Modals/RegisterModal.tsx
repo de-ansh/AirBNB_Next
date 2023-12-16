34355895564563f067f9ffd85a444bfb938586a8
Ashish Mishra <anshm9448@gmail.com>
2023-12-16 17:34:15 +0530

    added user register functionality

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import {FcGoogle} from "react-icons/fc";
-import {useCallback, useState} from 'react';
+import {useState} from 'react';
 
 import {
     FieldValues,
